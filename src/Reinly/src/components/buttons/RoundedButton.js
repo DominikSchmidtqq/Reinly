f9aa650ba00604a1e9327a4dd5a2f3121f0b9eb5
Dominik Schmidt <dominikschmidt1893@gmail.com>
2019-03-29 16:14:06 -0700

    Finished home screen UI

--- src/Reinly/src/components/buttons/RoundedButton.js
+++ src/Reinly/src/components/buttons/RoundedButton.js
@@ -10,10 +10,15 @@ import colors from "../../styles/colors"
 
 export default class RoundedButton extends Component {
     render() {
-        const { text } = this.props;
+        const { text, textColor, background, handleOnPress, icon } = this.props;
+        const backgroundColor = background || 'transparent';
+        const color = textColor || colors.black;
         return (
-            <TouchableHighlight style = {styles.wrapper}>
-                <Text style = {styles.buttonText}>{text}</Text>
+            <TouchableHighlight style = {[{backgroundColor}, styles.wrapper]} onPress = {handleOnPress} >
+                <View style = {styles.buttonTextWrapper}>
+                    {icon}
+                    <Text style = {[{color},styles.buttonText]}>{text}</Text>
+               </View>
             </TouchableHighlight>
         )
     }
@@ -21,6 +26,10 @@ export default class RoundedButton extends Component {
 
 RoundedButton.PropTyes = {
     text: PropTypes.string.isRequired,
+    textColor: PropTypes.string,
+    background: PropTypes.string,
+    handleOnPress: PropTypes.func.isRequired,
+
 };
 
 const styles = StyleSheet.create({
@@ -29,11 +38,17 @@ const styles = StyleSheet.create({
         padding: 15,
         borderRadius: 40,
         borderWidth: 1,
-        borderColor: colors.white
+        borderColor: colors.white,
+        marginBottom: 15,
+        alignItems: 'center',
     },
     buttonText: {
         fontSize: 17,
         width: '100%',
         textAlign: 'center'
+    },
+    buttonTextWrapper: {
+        flexDirection: 'row',
+        justifyContent: 'flex-end'
     }
 });
--- src/Reinly/src/screens/LoggedOut.js
+++ src/Reinly/src/screens/LoggedOut.js
@@ -1,18 +1,43 @@
 
 import React, {Component} from 'react';
 import colors from '../styles/colors';
-import {StyleSheet, Text,View, Image} from 'react-native'
+import {StyleSheet, Text,View, Image, TouchableHighlight} from 'react-native'
 import RoundedButton from '../components/buttons/RoundedButton';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default class LoggedOut extends Component {
+    onFacebookPress() {
+        alert('Facebook')
+    }
+
+    onCreateAccountPress() {
+        alert('Create Account')
+    }
+
+    onLoginPress() {
+        alert('Login Press')
+    }
+
     render() {
         return(
             <View style = {styles.wrapper}>
                 <View style = {styles.welcomeWrapper}>
-                    <Image source = {require('../images/reinly_logo.png')}
-                    style = {styles.logo}/>
+                    <Image source = {require('../images/reinly_logo.png')} style = {styles.logo}/>
                     <Text style = {styles.welcomeText}>Welcome to Reinly!</Text>
-                    <RoundedButton text = "Continue with Facebook"/>
+                    <RoundedButton text = "Continue with Facebook"
+                                   textColor = {colors.blue}
+                                   background = {colors.white}
+                                   icon = {<Icon name = "facebook" size = {20} style = {styles.facebookButtonIcon} />}
+                                   handleOnPress = {this.onFacebookPress}
+                    />
+                    <RoundedButton text = "Login"
+                                   textColor = {colors.white}
+                                   handleOnPress = {this.onLoginPress}
+                    />
+                    <RoundedButton text = "Create Account"
+                                   textColor = {colors.white}
+                                   handleOnPress = {this.onCreateAccountPress}
+                    />
                 </View>
             </View>
         );
@@ -42,5 +67,11 @@ const styles = StyleSheet.create({
         color: colors.white,
         fontWeight: '300',
         marginBottom: 40
+    },
+    facebookButtonIcon: {
+        color: colors.blue,
+        position: 'relative',
+        left: 20,
+        zIndex: 8
     }
 });
